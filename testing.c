d14fc6ca757d766e1fcfb002772c864617c22726
Lurker2p0 <124472805+Lurker2p0@users.noreply.github.com>
2025-05-06 14:12:40 -0700

    Adding a whole bunch of infrastructure to my library

---
+++ testing.c
@@ -0,0 +1,13 @@
+/*
+This file is for runnning the expected test for all of the challenges
+*/
+
+#include <stdlib.h>
+#include <stdint.h>
+
+int s1c1(){
+    char* input  = malloc(96*sizeof(char));
+    strcpy(input,"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    
+
+}
