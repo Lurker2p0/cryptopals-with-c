d914f044ffeabd0d2bd98b945951a3023cbfe539
Lurker2p0 <124472805+Lurker2p0@users.noreply.github.com>
2025-05-06 14:17:09 -0700

    moved everything into a src folder

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "setone.h"
+#include "testing.h"
+
+
+int main(){
+    printf("\nProgram is running:\n");
+    char x = bas64_int(0x1);
+    printf("%c\n", x);
+}
+
+
+//I need a function to take a string in here and 
